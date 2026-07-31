32cfbf9c81aed0e0032331687f5da3c75dd6cbfa
KeyurDhamecha <dhamechakeyur1@gmail.com>
2026-07-31 23:04:28 +0530

    CREATE A NAVBAR

---
+++ src/assets/assets.js
@@ -0,0 +1,14 @@
+import logo from "./logo2.png";
+import search_icon from "./search_icon.png";
+import profile_icon from "./profile_icon.png";
+import cart_icon from "./cart_icon.png"
+
+export const assets = {
+  logo,
+  search_icon,
+  profile_icon,
+  cart_icon
+};
+
+
+
---
+++ src/pages/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Productcart = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Productcart
